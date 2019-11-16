3e394fdefe44a93a3db286eebb88f7fc25ceb8ad
rafa_ <rafa_@LAPTOP-R651CVJB>
2019-11-16 18:31:14 -0200

    Commit Inicial

---
+++ Aplicacao/src/Replay.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * Classe responsável por ler o arquivo do replay.
+ * @author Equipe
+ *
+ */
+public class Replay {
+
+	public void PrintReplay() {
+		//Carrego meu arquivo
+		File file = new File("Replay.txt");
+		String linha ;
+		try {
+
+			if(!file.exists()) {
+				System.out.println("Ops ... Nenhum dado de jogo encontrado no momento :(" );
+				return;
+			}
+
+			//Prepara e lê o arquivo
+			FileReader ler = new FileReader(file.getAbsoluteFile());
+			BufferedReader reader = new BufferedReader(ler);	
+
+			if(reader.readLine() == null) {
+				System.out.println("Ops ... Nenhum dado de jogo encontrado no momento :(" );
+			}
+
+			while((linha = reader.readLine()) != null ) {
+				System.out.println(linha);
+			}			
+			reader.close();
+			System.in.read();
+		} catch (Exception e) {
+			System.out.println(e);
+			System.err.println("Erro no game :(");
+		}
+
+
+	}
+}
